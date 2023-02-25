2f2799be0a032555ed805d61ac01d209e0342f61
inieqr <70246193+inieqr@users.noreply.github.com>
2023-02-25 02:31:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,30 +24,18 @@ def main():
     """)
     
     # getting the input data from the user
-    col1, col2, col3, col4, col5 = st.columns(5)
 
-    with col1:
       Breathing_Problem = st.text_input('Do you have problems with breathing?')
-    with col2:
       Fever = st.text_input('Any fever?')
-    with col3:
       Dry_Cough = st.text_input('How about dry cough?')
-    with col4:
       Sore_throat = st.text_input('Do you feel like you have a sore throat?')
-    with col5:
       HyperTension = st.text_input('Do you experience hypertension?')
-    with col1:
       Abroad_travel = st.text_input('Travelled abroad recently?')
-    with col2:
       Contact_with_COVID_Patient = st.text_input('Have you been in contact with a COVID patient?')
-    with col3:
       Attended_Large_Gathering = st.text_input('Did you recently attend a large gathering?')
-    with col4:
       Visited_Public_Exposed_Places = st.text_input('Have you recently visited any public exposed places?')
-    with col5:
       Family_working_in_Public_Exposed_Places = st.text_input('Do you have a relative working in public exposed places?')
     
-
     
 
     # code for Prediction
